9385040014cbf062416264114065864f0bc21703
Anderson <macedcrazy@gmail.com>
2025-03-17 23:04:04 -0300

    feat: header criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
- export const App = () => {
-  
-  return (
-    <div className="App">
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import { Header } from "./components/Header";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
-      Teste
-    
-    </div>
+export const App: React.FC = () => {
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <div style={{ padding: '2rem' }}>
+          <Routes>            
+
+          </Routes>
+        </div>
+      </Router>
+
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,24 @@
-export {};
+import { AppBar, Button, Toolbar, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+
+export const Header: React.FC = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" sx={{flexGrow: 1}}>
+                    Meu Portfolio
+                </Typography>
+                <Button color="inherit" component={Link} to='/'>
+                    Inicio
+                </Button>
+                <Button color="inherit" component={Link} to='/skills'>
+                    Habilidades
+                </Button>
+                <Button color="inherit" component={Link} to='/contact'>
+                    Contato
+                </Button>
+            </Toolbar>
+        </AppBar>
+
+    )
+}
