0cb63e9390dccfe2c4481fb6c62d6c075fe99a27
Anderson <macedcrazy@gmail.com>
2025-03-17 22:39:28 -0300

    feat: theme criado

--- src/theme.ts
+++ src/theme.ts
@@ -1,1 +1,27 @@
-export {};
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme ({
+    palette: {
+        primary: {
+            main: '#1976d2', //Azul padrão
+        },
+        secondary: {
+            main: '#ff5722', //Laranja para destaque
+        }
+    },
+
+    typography: {
+        h1: {
+            fontSize: '#2rem',
+            fontWeight: 'bold',
+        },
+        body1: {
+            fontSize: '1rem',
+        },
+    },
+});
+
+
+
+export default theme;
